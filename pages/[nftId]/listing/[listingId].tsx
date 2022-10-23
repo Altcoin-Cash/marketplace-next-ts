e0c25ad2db00512a98c8bb056d011f856f0c536e
Altcoin Cash <altcoincash@protonmail.com>
2022-10-23 17:18:25 +0200

    Update [listingId].tsx

--- pages/[nftId]/listing/[listingId].tsx
+++ pages/[nftId]/listing/[listingId].tsx
@@ -26,7 +26,7 @@ const ListingPage: NextPage = () => {
   const [, switchNetwork] = useNetwork();
 
   // Initialize the marketplace contract
-  const { contract: marketplace } = useContract<Marketplace>(
+  const { contract: marketplace } = useContract(
     "0xD0804F2cDFC75A308d786DcA78f0DC617d991CaE" // Your marketplace contract address here
   );
 
